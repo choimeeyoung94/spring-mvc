5e67c1a5324386d63bacc402560a75c3d82998ac
choimeeyoung94 <choimeeyoung2@gmail.com>
2025-08-07 12:38:33 +0900

    pagination change

--- 07_pagination/src/main/java/org/shark/pagination/repository/UserDAO.java
+++ 07_pagination/src/main/java/org/shark/pagination/repository/UserDAO.java
@@ -1,6 +1,7 @@
 package org.shark.pagination.repository;
 
 import java.util.List;
+import java.util.Map;
 
 import org.mybatis.spring.SqlSessionTemplate;
 import org.shark.pagination.model.dto.PageDTO;
@@ -15,8 +16,8 @@ public class UserDAO {
   
   private final SqlSessionTemplate sqlSession;
   
-  public List<UserDTO> getUsers(PageDTO dto) {
-    return sqlSession.selectList("mybatis.mapper.userMapper.getUsers", dto);
+  public List<UserDTO> getUsers(Map<String, Object> map) {
+    return sqlSession.selectList("mybatis.mapper.userMapper.getUsers", map);
   }
   
   public Integer getUserCount() {
--- 07_pagination/src/main/java/org/shark/pagination/service/UserServiceImpl.java
+++ 07_pagination/src/main/java/org/shark/pagination/service/UserServiceImpl.java
@@ -24,18 +24,19 @@ public class UserServiceImpl implements UserService {
   @Override
   public Map<String, Object> getUsers(PageDTO dto, HttpServletRequest request) {
     // 파라미터 sort 받기 (디폴트 DESC)
-    Optional<String> optSort = Optional.ofNullable(request.getParameter("sort"));
-    String sort = optSort.orElse("DESC");
-    
-    
-    
+    //Optional<String> optSort = Optional.ofNullable(request.getParameter("sort"));
+   // sort = optSort.orElse("DESC");
+    String sort = request.getParameter("sort");
+    if (sort == null || !(sort.equalsIgnoreCase("ASC") || sort.equalsIgnoreCase("DESC"))) {
+      sort = "DESC";
+    }
     
     // 전체 항목의 개수를 PageDTO 객체에 저장
     int itemCount = userDAO.getUserCount();
     dto.setItemCount(itemCount);
     // 페이징 정보 계산해서 PageDTO 객체에 저장하기 (PageDTO 객체에 페이징 위한 모든 정보가 저장된다)
     pageUtil.calculatePaging(dto);
-    List<UserDTO> users = userDAO.getUsers(dto);
+    List<UserDTO> users = userDAO.getUsers(Map.of("offset", dto.getOffset(), "size", dto.getSize(), "sort", sort));
     String pagingHTML = pageUtil.getPaginHtml(dto, request.getContextPath() +  "/user/list", null);
     // 결과 반환
     return Map.of("users", users, "pagingHTML", pagingHTML);
